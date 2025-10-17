634c2e2304112165dd1f52494e29790d1424c4e5
Justus Katerberg <j.katerberg@qbud.nl>
2025-10-17 16:03:00 +0200

    Improve placeholder

--- src/components/search-input.js
+++ src/components/search-input.js
@@ -39,7 +39,7 @@ class SearchInput {
           maxlength="100"
           autocomplete="off"
         />
-        <span id="tommy-search-placeholder">Accommodatie zoeken</span>
+        <span id="tommy-search-placeholder">Boek hier uw vakantie</span>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M9.145 18.29c-5.042 0-9.145-4.102-9.145-9.145s4.103-9.145 9.145-9.145 9.145 4.103 9.145 9.145-4.102 9.145-9.145 9.145zm0-15.167c-3.321 0-6.022 2.702-6.022 6.022s2.702 6.022 6.022 6.022 6.023-2.702 6.023-6.022-2.702-6.022-6.023-6.022zm9.263 12.443c-.817 1.176-1.852 2.188-3.046 2.981l5.452 5.453 3.014-3.013-5.42-5.421z"/></svg>
         <span class="loader"></span>
       </div>
